fe5393d82d36a438ee61720b3a9c729ce0836352
An La <a.ngoc.la@gmail.com>
2016-02-04 18:16:25 -0800

    my blog inital commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,10 @@
+class HomesController < ApplicationController
+
+  # This controller will handle the homes and static pages
+  def index
+    # render text: "Welcome!"
+  end
+
+  def about
+  end
+end
